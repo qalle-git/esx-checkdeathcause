e55a71653eccbfbaeb90009f2f823afe181aa7d9
Marcos Frutos <marcos.frutos@helloprint.com>
2020-02-13 14:48:43 +0100

    adding spanish translation (locale:es)

---
+++ translation/es.lua
@@ -0,0 +1,16 @@
+Locales['es'] = {
+        ['press_e'] = 'Presione ~INPUT_CONTEXT~ para comprobar como ~r~murió~s~ of',
+      ['hardmeele'] = 'Probablemente por un golpe en la cabeza',
+      ['bullet'] = 'Probablemente a disparos',
+      ['knifes'] = 'Probablemente con arma blanca',
+      ['bitten'] = 'Probablemente mordido por un animal',
+      ['brokenlegs'] = 'Probablemente caídas, rompiéndose ambas piernas',
+      ['explosive'] = 'Probablemente murió por un artefacto explosivo',
+      ['gas'] = 'Probablemente por un problema pulmonar',
+      ['fireextinguisher'] = 'Probablemente murió por el gas de un extinctor',
+      ['fire'] = 'Probablemente murió en un fuego',
+      ['caraccident'] = 'Probablemente murió en un accidente',
+      ['drown'] = 'Probablemente se ahogó',
+      ['unknown'] = 'Muerte desconocida',
+  }
+  
